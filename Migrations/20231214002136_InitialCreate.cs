7df49c1b8351119739cdb278bb09e80543944c90
Andrew <laxmeyers@gmail.com>
2023-12-13 17:33:13 -0700

    added the link to go to pages

---
+++ Migrations/20231214002136_InitialCreate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RazorPagesSacrament.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Meeting",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ConductingLeader = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    OpeningPrayer = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ClosingPrayer = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Speaker1 = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Speaker2 = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Speaker3 = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    OpeningHymn = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ClosingHymn = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IntermediateHymn = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Meeting", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Meeting");
+        }
+    }
+}
